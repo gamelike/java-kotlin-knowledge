571e4ba53e835c64a2a081f86c6190107d0119e8
violet <zhao_yifan@topsec.com.cn>
2022-05-15 20:06:17 +0800

    feat(jdk11): variables infer and http client

---
+++ src/main/java/jdk/jdk11/FunctionInterface.java
@@ -0,0 +1,11 @@
+package jdk.jdk11;
+
+/**
+ * @author violet.
+ */
+@FunctionalInterface
+public interface FunctionInterface {
+
+    String processor(impl a, String b);
+
+}
---
+++ src/main/java/jdk/jdk11/impl.java
@@ -0,0 +1,11 @@
+package jdk.jdk11;
+
+/**
+ * @author violet.
+ */
+public class impl implements FunctionInterface {
+    @Override
+    public String processor(String a) {
+        return a;
+    }
+}
