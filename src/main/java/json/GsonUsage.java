c48784eb73b27c534e87a34760ac16c04a35bbb1
violet <zhao_yifan@topsec.com.cn>
2022-05-27 09:57:05 +0800

    feat(gson): add ParserJson demo

---
+++ src/main/java/json/GsonUsage.java
@@ -0,0 +1,31 @@
+package json;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author violet.
+ */
+@Slf4j
+public class GsonUsage {
+
+    public static void ParserJson(String s) {
+        JsonElement jsonElement = JsonParser.parseString(s);
+        if (jsonElement.isJsonObject()) {
+            log.info("jsonElement: {}", jsonElement.getAsJsonObject());
+        } else if (jsonElement.isJsonArray()) {
+            log.info("jsonElement: {}", jsonElement.getAsJsonArray());
+        } else {
+            log.info("jsonElement: {}", jsonElement.getAsString());
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = """
+               abc
+               """;
+        ParserJson(str);
+    }
+
+}
