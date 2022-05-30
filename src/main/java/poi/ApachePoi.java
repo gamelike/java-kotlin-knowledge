155fba65d9f700aeebd29c50e3eae37794426173
violet <zhao_yifan@topsec.com.cn>
2022-05-31 01:24:45 +0800

    chore(poi): import maven poi operate office.

---
+++ src/main/java/poi/ApachePoi.java
@@ -0,0 +1,14 @@
+package poi;
+
+/**
+ * document: <a href="https://poi.apache.org/apidocs/dev/index.html">apache poi</a>
+ *
+ * @author violet.
+ */
+public class ApachePoi {
+
+    public void create_word() {
+
+    }
+
+}
