1647668045954b41d569b75edc7b3fa3118eb483
violet <zhao_yifan@topsec.com.cn>
2022-06-08 23:20:23 +0800

    feat(leetcode): three points are all distinct and not in a straight line.

---
+++ src/main/java/leetcode/Day20220608.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * easy.
+ */
+public class Day20220608 {
+
+    public boolean isBoomerang(int[][] points) {
+        /*
+            y1 = k x1 + b;
+            y2 = k x2 + b;
+            k = (y2 - y1)/(x2 - x1);
+         */
+        return (points[0][1] - points[1][1]) * (points[2][0] - points[0][0]) !=
+                (points[2][1] - points[0][1]) * (points[0][0] - points[1][0]);
+    }
+
+}
