db7b0d5f171e1373929625f4525472a1e29651b7
violet <zhao_yifan@topsec.com.cn>
2022-07-13 22:21:15 +0800

    ci(fix): 修复缺少Dto.

--- src/test/java/com/BootstrapApplicationSmokingTest.java
+++ src/test/java/com/BootstrapApplicationSmokingTest.java
@@ -1,13 +1,10 @@
 package com;
 
-import annotation.demo.Dto;
 import application.infrastructure.repository.HistoryRepository;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.model.BulkOperationDTO;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
+import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
@@ -79,6 +76,63 @@ public class BootstrapApplicationSmokingTest {
                                 """;
         BulkOperationDTO dto = objectMapper.readValue(res.getBytes(StandardCharsets.UTF_8), BulkOperationDTO.class);
         log.info("{}", dto);
+    }
+
+    @Test
+    @SneakyThrows
+    public void jackson_no_creator() {
+        String json = """
+                {
+                "id":"123",
+                "result":0,
+                "influence":["abc"]
+                }
+                """;
+        Object o = objectMapper.readerFor(RequestDto.class).readValue(json);
+        log.info("{}", o);
+    }
+
+    @Getter
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @ToString
+    static class RequestDto {
+        final String id;
+        final int result;
+        final String description;
+        final List<String> influence;
+
+        @JsonCreator
+        RequestDto(@JsonProperty("id") String id, @JsonProperty("result") int result, @JsonProperty("description") String description,
+                   @JsonProperty("influence") List<String> influence) {
+            this.id = id;
+            this.result = result;
+            this.description = description;
+            this.influence = influence;
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    public void jackson_creator_test() {
+        String json = """
+                {
+                "id":"123",
+                "result":0,
+                "influence":["abc"]
+                }
+                """;
+        ResponseDto o = objectMapper.readValue(json.getBytes(StandardCharsets.UTF_8), ResponseDto.class);
+        log.info("{}", o);
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    static class ResponseDto {
+        private String id;
+        private int result;
+        private String description;
+        private List<String> influence;
     }
 
 }
