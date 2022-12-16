8737b372f44e40cb193798808433267cf35e9e54
郭敬冬 <gjd2019@126.com>
2022-12-16 16:42:45 +0800

    feat:对象值未赋值

--- src/test/java/org/springframework/test/service/WorldService.java
+++ src/test/java/org/springframework/test/service/WorldService.java
@@ -5,4 +5,6 @@ package org.springframework.test.service;
  */
 public interface WorldService {
   void explode();
+
+  String getName();
 }
--- src/test/java/org/springframework/test/service/WorldServiceImpl.java
+++ src/test/java/org/springframework/test/service/WorldServiceImpl.java
@@ -4,8 +4,20 @@ package org.springframework.test.service;
  * @author gjd
  */
 public class WorldServiceImpl implements WorldService {
+
+  private String name;
+
   @Override
   public void explode() {
-    System.out.println("this is explode function");
+    System.out.println("this " + name + " is explode function");
+  }
+
+  @Override
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
   }
 }
