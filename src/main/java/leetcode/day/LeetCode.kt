576674daaeb49ea1c854c02713b2a26c77916582
violet <a289392424@gmail.com>
2023-08-05 23:45:17 +0800

    merge linked list leetcode 2023/08/05

--- src/main/java/leetcode/day/LeetCode.kt
+++ src/main/java/leetcode/day/LeetCode.kt
@@ -1,8 +1,6 @@
 package leetcode.day
 
-/**
- *
- */
+val directions = arrayOf(arrayOf(0, 1), arrayOf(1, 0), arrayOf(-1, 0), arrayOf(0, -1))
 fun uniquePathsIII(grid: Array<IntArray>): Int {
     var count = 0
     var startPointX = 0
@@ -18,28 +16,39 @@ fun uniquePathsIII(grid: Array<IntArray>): Int {
             count++
         }
     }
+    fun dfsUniquePathsIII(grid: Array<IntArray>, i: Int, j: Int, n: Int): Int {
+        if (grid[i][j] == 2) {
+            return if (n != 0) 0 else 1
+        }
+        var number = 0
+        val tem = grid[i][j]
+        grid[i][j] = -1
+        for (direction in directions) {
+            val nextI = i + direction[0]
+            val nextJ = j + direction[1]
+            if (nextI < grid.size && nextJ < grid[0].size && nextJ >= 0 && nextI >= 0 && grid[nextI][nextJ] != -1) {
+                number += dfsUniquePathsIII(grid, nextI, nextJ, n - 1)
+            }
+        }
+        grid[i][j] = tem
+        return number
+    }
     return dfsUniquePathsIII(grid, startPointX, startPointY, count - 1)
 }
 
-val directions = arrayOf(arrayOf(0, 1), arrayOf(1, 0), arrayOf(-1, 0), arrayOf(0, -1))
-fun dfsUniquePathsIII(grid: Array<IntArray>, i: Int, j: Int, n: Int): Int {
-    if (grid[i][j] == 2) {
-        return if (n != 0) 0 else 1
-    }
-    var count = 0
-    val tem = grid[i][j]
-    grid[i][j] = -1
-    for (direction in directions) {
-        val nextI = i + direction[0]
-        val nextJ = j + direction[1]
-        if (nextI < grid.size && nextJ < grid[0].size && nextJ >= 0 && nextI >= 0 && grid[nextI][nextJ] != -1) {
-            count += dfsUniquePathsIII(grid, nextI, nextJ, n - 1)
-        }
-    }
-    grid[i][j] = tem
-    return count
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
 }
 
-fun main() {
-    println(uniquePathsIII(arrayOf(intArrayOf(1, 0, 0, 0), intArrayOf(0, 0, 0, 0), intArrayOf(0, 0, 2, -1))))
+fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+    return if (list1 == null) return list2 else if (list2 == null) list1
+    else {
+        if (list1.`val` <= list2.`val`) {
+            list1.next = mergeTwoLists(list1.next, list2)
+            list1
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next)
+            list2
+        }
+    }
 }
